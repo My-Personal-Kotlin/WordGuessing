d1d8643bf64b7853d423b1ab00cf640cbd45d893
Awais Mansha <awaismansha1998@gmail.com>
2021-09-08 22:34:38 +0500

    working on Lesson 5 in Kotlin

---
+++ app/src/main/java/com/wordguessing/screens/title/TitleFragment.kt
@@ -0,0 +1,29 @@
+package com.wordguessing.screens.title
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.wordguessing.R
+import com.wordguessing.databinding.TitleFragmentBinding
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: TitleFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.title_fragment, container, false)
+
+        binding.playGameButton.setOnClickListener {
+            findNavController().navigate(TitleFragmentDirections.actionTitleToGame())
+        }
+        return binding.root
+    }
+
+}
