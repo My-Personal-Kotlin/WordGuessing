c30e0233c567aa9273db129173628bba03badaec
Awais Mansha <awaismansha1998@gmail.com>
2021-09-09 18:16:40 +0500

    working on Lesson 5 in Kotlin

--- app/src/main/java/com/wordguessing/screens/game/GameFragment.kt
+++ app/src/main/java/com/wordguessing/screens/game/GameFragment.kt
@@ -1,32 +1,29 @@
 package com.wordguessing.screens.game
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.ui.text.toUpperCase
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.wordguessing.R
 import com.wordguessing.databinding.GameFragmentBinding
-import java.util.*
 
 class GameFragment : Fragment() {
 
-    // The current word
-    private var word = ""
-
-    // The current score
-    private var score = 0
-
-    // The list of words - the front of the list is the next word to guess
-    private lateinit var wordList: MutableList<String>
+    private lateinit var gameViewModel: GameViewModel
 
     private lateinit var binding: GameFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+        Log.v("GameViewModel","ViewModelProvider called this ! ")
+        gameViewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
         // Inflate view and obtain an instance of the binding class
         binding = DataBindingUtil.inflate(
@@ -36,92 +33,33 @@ class GameFragment : Fragment() {
             false
         )
 
-        resetList()
-        nextWord()
+        binding.correctButton.setOnClickListener {
+            gameViewModel.onCorrect()
+        }
+        binding.skipButton.setOnClickListener {
+            gameViewModel.onSkip()
+        }
 
-        binding.correctButton.setOnClickListener { onCorrect() }
-        binding.skipButton.setOnClickListener { onSkip() }
+        gameViewModel.score.observe(viewLifecycleOwner, Observer {
+            binding.scoreText.text = "Score : ${it}"
+        })
+        gameViewModel.word.observe(viewLifecycleOwner, Observer {
+            binding.wordText.text = it.uppercase()
+        })
 
-//        updateScoreText()
-//        updateWordText()
         return binding.root
 
     }
 
-    /**
-     * Resets the list of words and randomizes the order
-     */
-    private fun resetList() {
-        wordList = mutableListOf(
-            "queen",
-            "hospital",
-            "basketball",
-            "cat",
-            "change",
-            "snail",
-            "soup",
-            "calendar",
-            "sad",
-            "desk",
-            "guitar",
-            "home",
-            "railway",
-            "zebra",
-            "jelly",
-            "car",
-            "crow",
-            "trade",
-            "bag",
-            "roll",
-            "bubble"
-        )
-        wordList.shuffle() // thats why we have to set it to MutableList
-    }
+
 
     /**
      * Called when the game is finished
      */
     private fun gameFinished() {
-        val action = GameFragmentDirections.actionGameToScore(score)
+        val action = GameFragmentDirections.actionGameToScore(
+            gameViewModel.score.value?:0)
         findNavController().navigate(action)
-    }
-
-    /**
-     * Moves to the next word in the list
-     */
-    private fun nextWord() {
-        //Select and remove a word from the list
-        if (wordList.isEmpty()) {
-            gameFinished()
-        } else {
-            word = wordList.removeAt(0)
-        }
-        updateWordText()
-        updateScoreText()
-        binding.invalidateAll()
-    }
-
-    /** Methods for buttons presses **/
-
-    private fun onSkip() {
-        score--
-        nextWord()
-    }
-
-    private fun onCorrect() {
-        score++
-        nextWord()
-    }
-
-    /** Methods for updating the UI **/
-
-    private fun updateWordText() {
-        binding.wordText.text = word.uppercase()
-
-    }
-
-    private fun updateScoreText() {
-        binding.scoreText.text = "Score : ${score}"
     }
 
 }
